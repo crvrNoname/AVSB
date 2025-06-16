3f1cd7980b052411470f02db1a7d04516c4e7f9e
crvrNoname <nonamecrvr@gmail.com>
2025-06-16 16:31:41 -0400

    mejoras movile

--- src/js/modules/navbar.js
+++ src/js/modules/navbar.js
@@ -2,5 +2,6 @@ const toggleButton = document.getElementById('toggle-menu');
 const menu = document.getElementById('navbar-menu');
 
 toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('open');
+    menu.classList.toggle('open'); // Alternar clase 'open' en el menú
+    // Mantener siempre el texto "Menú" (sin cambios)
 });
